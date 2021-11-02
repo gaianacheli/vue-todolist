41d7aa1dd4731da75a455b03e9aab64762357aa3
gaianacheli <gaianacheli04@gmail.com>
2021-11-02 14:29:53 +0100

    creata funzione al click di button

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,7 @@
 const app = new Vue ({
     el:"#app",
     data:{
+        newTask: "",
         tasks: [
             'Fare la spesa',
             'Pagare bollette',
@@ -11,6 +12,12 @@ const app = new Vue ({
         removeTask(i){
            // console.log('rimosso',i);
             this.tasks.splice(i,1)
+        },
+        addTask(){
+            if(this.newTask.length > 5){
+                this.tasks.push(this.newTask)
+            }
+            this.newTask= ""
         }
     }
 })
